3985387ad14ba870aa67a6ecb5f928b38866c3b1
qubbit <gopala@email.arizona.edu>
2018-07-28 01:57:28 -0400

    Theme colors

--- src/App.js
+++ src/App.js
@@ -5,20 +5,32 @@ import './styles/main.css';
 import { Route } from 'react-router-dom';
 
 class App extends Component {
+  changeTheme(theme) {
+    this.mainRef.classList = [theme];
+  }
+
   render() {
     return (
-      <div id="canvas">
-        <header>
-          <a href="/">
-            <h1 logo="logo">
-              <span className="first-name">Gopal</span> Adhikari
-            </h1>
-          </a>
-          <small>Software Developer | Not A Photographer</small>
-        </header>
-        <Route path="/" component={Albums} exact />
-        <Route path="/albums/:id" component={AlbumPhotos} exact />
-        <footer>&copy; Gopal Adhikari 2018. Photos powered by Flickr</footer>
+      <div id="main" className="light" ref={ref => (this.mainRef = ref)}>
+        <div id="canvas">
+          <header>
+            <div className="masthead">
+              <a href="/">
+                <h1 logo="logo">
+                  <span className="first-name">Gopal</span> Adhikari
+                </h1>
+              </a>
+              <small>Software Developer | Not A Photographer</small>
+            </div>
+            <div className="theme-changer">
+              <button onClick={() => this.changeTheme('light')}>Light</button>
+              <button onClick={() => this.changeTheme('dark')}>Dark</button>
+            </div>
+          </header>
+          <Route path="/" component={Albums} exact />
+          <Route path="/albums/:id" component={AlbumPhotos} exact />
+          <footer>&copy; Gopal Adhikari 2018. Photos powered by Flickr</footer>
+        </div>
       </div>
     );
   }
